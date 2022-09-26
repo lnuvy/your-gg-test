3ce086c31e2ba94fe63be05552bf80c72a2d6267
lnuvy <lnuvy.code@gmail.com>
2022-09-26 22:18:08 +0900

    frame setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,18 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from '../styles/theme'
 import GlobalStyles from '../styles/GlobalStyles'
+import { QueryClientProvider } from '@tanstack/react-query/src/QueryClientProvider'
+import { QueryClient } from '@tanstack/query-core'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
